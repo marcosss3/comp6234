2fe066353a786c7b02035720eedac321d4e2437f
jackey <jackey1510@gmail.com>
2017-01-09 16:41:18 +0000

    Currency chart

--- public/javascript/currency_chart/currencyChart.controller.js
+++ public/javascript/currency_chart/currencyChart.controller.js
@@ -14,7 +14,8 @@
         .controller('currencyChartController',  currencyChartController);
 
 		
-    function currencyChartController($scope) {				
+    function currencyChartController($scope) {		
+		//refers to http://bl.ocks.org/DStruths/9c042e3a6b66048b5bd4
 		var margin = {top: 20, right: 200, bottom: 100, left: 90},
 			margin2 = { top: 430, right: 10, bottom: 20, left: 80 },
 			width = 1000 - margin.left - margin.right,
@@ -52,6 +53,8 @@
 			.attr("height", height + margin.top + margin.bottom)
 		  .append("g")
 			.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+			
+		
 
 		// Create invisible rect for mouse tracking
 		svg.append("rect")
@@ -111,39 +114,39 @@
 			  .text("Change in Exchange Rate(%)");
 
 		  var currency = svg.selectAll(".currency")
-			  .data(categories) // Select nested data and append to new svg group elements
+			  .data(categories) // Select nested data
 			.enter().append("g")
 			  .attr("class", "currency");   
 
 		  currency.append("path")
 			  .attr("class", "line")
-			  .style("pointer-events", "none") // Stop line interferring with cursor
+			  .style("pointer-events", "none")
 			  .attr("id", function(d) {
-				return "line-" + d.name.replace(" ", "").replace("/", ""); // Give line id of line-(insert currency name, with any spaces replaced with no spaces)
+				return "line-" + d.name.replace(" ", "").replace("/", ""); 
 			  })
 			  .attr("d", function(d) { 
 				return line(d.values); 
 			  })
-			  .attr("clip-path", "url(#clip)")//use clip path to make irrelevant part invisible
+			  .attr("clip-path", "url(#clip)")
 			  .style("stroke", function(d) { return color(d.name) })
 			  .style("opacity", function(d) { return d.visible ? '1': '0.1'; });
 			  
 
 		  // draw legend
-		  var legendSpace = 450 / categories.length; // 450/number of issues (ex. 40)    
+		  var legendSpace = 450 / categories.length;   
 
 		  currency.append("rect")
 			  .attr("width", 10)
 			  .attr("height", 10)                                    
 			  .attr("x", width + (margin.right/3) - 10) 
-			  .attr("y", function (d, i) { return i*(legendSpace) - 9; })  // spacing
+			  .attr("y", function (d, i) { return i*(legendSpace) - 9; })  
 			  .attr("fill",function(d) {
-				return d.visible ? color(d.name) : "#F1F1F2"; // If array key "visible" = true then color rect, if not then make it grey 
+				return d.visible ? color(d.name) : "#F1F1F2"; 
 			  })
 			  .attr("class", "legend-box")
 
-			  .on("click", function(d){ // On click make d.visible 
-				d.visible = !d.visible; // If array key for this data selection is "visible" = true then make it false, if false then make it true
+			  .on("click", function(d){ 
+				d.visible = !d.visible; 
 
 				svg.select(".y.axis")
 				  .transition()
@@ -190,7 +193,7 @@
 			  
 		  currency.append("text")
 			  .attr("x", width + (margin.right/3)) 
-			  .attr("y", function (d, i) { return i*(legendSpace); })  // (return (11.25/2 =) 5.625) + i * (5.625) 
+			  .attr("y", function (d, i) { return i*(legendSpace); })  
 			  .text(function(d) { return d.name; }); 
 		  
 
@@ -203,71 +206,60 @@
 					.attr("id", "hover-line")
 					.attr("x1", 10).attr("x2", 10) 
 					.attr("y1", 0).attr("y2", height + 10)
-					.style("pointer-events", "none") // Stop line interferring with cursor
-					.style("opacity", 1e-6); // Set opacity to zero 
+					.style("pointer-events", "none")
+					.style("opacity", 1e-6); 
 
 		  var hoverDate = hoverLineGroup
 				.append('text')
 					.attr("class", "hover-text")
-					.attr("y", height - (height-40)) // hover date text position
-					.attr("x", width - 150) // hover date text position
+					.attr("y", height - (height-40))
+					.attr("x", width - 150) 
 					.style("fill", "#E6E7E8");
 
-		  var columnNames = d3.keys(data[0]) //grab the key values from your first data row
-											 //these are the same as your column names
-						  .slice(1); //remove the first column name (`date`);
+		  var columnNames = d3.keys(data[0]) 
+						  .slice(1); //remove date
 
-		  var focus = currency.select("g") // create group elements to house tooltip text
-			  .data(columnNames) // bind each column name date to each g element
-			.enter().append("g") //create one <g> for each columnName
+		  var focus = currency.select("g") 
+			  .data(columnNames) 
+			.enter().append("g") 
 			  .attr("class", "focus"); 
 
-		  focus.append("text") // http://stackoverflow.com/questions/22064083/d3-js-multi-series-chart-with-y-value-tracking
+		  focus.append("text") 
 				.attr("class", "tooltips")
 				.attr("x", width + 10) // position tooltips  
-				.attr("y", function (d, i) { return i*(legendSpace); }) // (return (11.25/2 =) 5.625) + i * (5.625) // position tooltips   
+				.attr("y", function (d, i) { return i*(legendSpace); })
 				
-		  // Add mouseover events for hover line.
-		  d3.select("#mouse-tracker") // select chart plot background rect #mouse-tracker
-		  .on("mousemove", mousemove) // on mousemove activate mousemove function defined below
+		  d3.select("#mouse-tracker") 
+		  .on("mousemove", mousemove) 
 		  .on("mouseout", function() {
 			  hoverDate
-				  .text(null) // on mouseout remove text for hover date
+				  .text(null) 
 
 			  d3.select("#hover-line")
-				  .style("opacity", 1e-6); // On mouse out making line invisible
+				  .style("opacity", 1e-6); 
 		  });
-
+		  
 		  function mousemove() { 
-			  var mouse_x = d3.mouse(this)[0]; // Finding mouse x position on rect
+			  var mouse_x = d3.mouse(this)[0]; 
 			  var graph_x = xScale.invert(mouse_x); // 
 			  
 			  var format = d3.time.format('%d %b %y');
 			  
-			  hoverDate.text(format(graph_x)); // scale mouse position to xScale date and format it to show month and year
+			  hoverDate.text(format(graph_x)); 
 			  
-			  d3.select("#hover-line") // select hover-line and changing attributes to mouse position
+			  d3.select("#hover-line") 
 				  .attr("x1", mouse_x) 
 				  .attr("x2", mouse_x)
-				  .style("opacity", 1); // Making line visible
+				  .style("opacity", 1); 
 
-			  // Legend tooltips // http://www.d3noob.org/2014/07/my-favourite-tooltip-method-for-line.html
-
-			  var x0 = xScale.invert(d3.mouse(this)[0]), /* d3.mouse(this)[0] returns the x position on the screen of the mouse. xScale.invert function is reversing the process that we use to map the domain (date) to range (position on screen). So it takes the position on the screen and converts it into an equivalent date! */
-			  i = bisectDate(data, x0, 1), // use our bisectDate function that we declared earlier to find the index of our data array that is close to the mouse cursor
-			  /*It takes our data array and the date corresponding to the position of or mouse cursor and returns the index number of the data array which has a date that is higher than the cursor position.*/
+			  var x0 = xScale.invert(d3.mouse(this)[0]), 
+			  i = bisectDate(data, x0, 1),
+			
 			  d0 = data[i - 1],
 			  d1 = data[i],
-			  /*d0 is the combination of date and rating that is in the data array at the index to the left of the cursor and d1 is the combination of date and close that is in the data array at the index to the right of the cursor. In other words we now have two variables that know the value and date above and below the date that corresponds to the position of the cursor.*/
+			 
 			  d = x0 - d0.Date > d1.Date - x0 ? d1 : d0;
-			  /*The final line in this segment declares a new array d that is represents the date and close combination that is closest to the cursor. It is using the magic JavaScript short hand for an if statement that is essentially saying if the distance between the mouse cursor and the date and close combination on the left is greater than the distance between the mouse cursor and the date and close combination on the right then d is an array of the date and close on the right of the cursor (d1). Otherwise d is an array of the date and close on the left of the cursor (d0).*/
-
-			  //d is now the data row for the date closest to the mouse position
-
 			  focus.select("text").text(function(columnName){
-				 //because you didn't explictly set any data on the <text>
-				 //elements, each one inherits the data from the focus <g>
-
 				 return (d[columnName]+'%');
 			  });
 		  }; 
@@ -276,11 +268,11 @@
 		  var button2 = d3.select("#none").on('click',hide); // Select none
 		  
 		  function show(){
-			categories = color.domain().map(function(name) { // Nest the data into an array of objects with new keys
+			categories = color.domain().map(function(name) {
 
 			return {
-			  name: name, // "name": the csv headers except date
-			  values: data.map(function(d) { // "values": which has an array of the dates and ratings
+			  name: name, // 
+			  values: data.map(function(d) { 
 				return {
 				  Date: +d.Date, 
 				  Rate: +(d[name])/100,
@@ -292,10 +284,10 @@
 		  refresh();
 		  }
 		  function hide(){
-			categories = color.domain().map(function(name) { // Nest the data into an array of objects with new keys
+			categories = color.domain().map(function(name) { 
 			return {
-			  name: name, // "name": the csv headers except date
-			  values: data.map(function(d) { // "values": which has an array of the dates and ratings
+			  name: name, 
+			  values: data.map(function(d) { 
 				return {
 				  Date: +d.Date, 
 				  Rate: +(d[name])/100,
@@ -312,7 +304,7 @@
 			.transition()
 			.style("pointer-events", "none")
 			.attr("d", function(d){
-				return line(d.values); // If d.visible is true then draw line for this d selection
+				return line(d.values); 
 			  })
 			  .style("opacity", function(d) { return d.visible ? '1': '0.1'; });
 
@@ -342,6 +334,6 @@
 		
 		d3.select("#currencychart").attr("align","center");
 		  
-		}); // End Data callback function
+		}); // End
 	}
 })();
